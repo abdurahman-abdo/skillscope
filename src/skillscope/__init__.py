1365ccd945d1ab9c56e060d034f89b405b2236fb
Abdurahman Mohammed <abdurahmanmoh.3@gmail.com>
2026-09-04 16:24:13 +0300

    First commit: Collected all the data and stored it in src/data/cleaned

---
+++ src/skillscope/__init__.py
@@ -0,0 +1,6 @@
+def main() -> None:
+    print("""This is the __inti__ file of the skillScope package.\n
+            You can tweak with the main.ipynb notebook to see the output of the skillScope package.""")
+
+if __name__ == "__main__":
+    main()
